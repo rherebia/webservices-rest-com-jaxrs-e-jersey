495c0abc1716506e61bddbdd6c9c82695c6b92f5
Renato Herebia <rherebia@gmail.com>
2017-01-24 09:16:43 -0300

    Assistindo aula 4

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -14,10 +14,10 @@ public class CarrinhoResource {
 
 	@Path("{id}")
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public String busca(@PathParam("id") Long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		
-		return carrinho.toXML();
+		return carrinho.toJson();
 	}
 }
